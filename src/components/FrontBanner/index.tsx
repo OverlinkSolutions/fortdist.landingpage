32a4e740ab10d71ecec338a737e61bfd47f510ad
Erick Oliveira <erickoliveiraah@gmail.com>
2024-09-09 09:07:48 -0300

    remover banner antigo

--- src/components/FrontBanner/index.tsx
+++ src/components/FrontBanner/index.tsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from 'react';
 
 import banner1 from '../../assets/banner1.png';
 import banner1Mobile from '../../assets/banner1-mobile.png';
-import banner2 from '../../assets/banner2.png';
-import banner2Mobile from '../../assets/banner2-mobile.png';
 import { getWindowDimensions } from '../../utils/dimensions';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import { Carousel } from 'react-responsive-carousel';
 
 import "./styles.css";
 
@@ -30,13 +28,12 @@ export default function FrontBanner() {
 	const isMobile = useWindowDimensions().width < 500;
 
 	return (
-		<Carousel infiniteLoop swipeable showThumbs={false} autoPlay interval={5000} showStatus={false}>
-			<div>
-				<img className='d-block w-100' src={isMobile ? banner2Mobile : banner2} alt="Banner 2" />
-			</div>
+		// <Carousel infiniteLoop swipeable showThumbs={false} autoPlay interval={5000} showStatus={false}>
+			
 			<div>
 				<img className='d-block w-100' src={isMobile ? banner1Mobile : banner1} alt="Banner um" />
 			</div>
-		</Carousel>
+			
+		// </Carousel>
 	);
 }
