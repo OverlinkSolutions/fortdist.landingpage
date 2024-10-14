bdfc271a5ede52ca7c5f63711432f8cff131abbe
Erick Oliveira <erickoliveiraah@gmail.com>
2024-10-14 11:56:25 -0300

    update font size

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -7,29 +7,17 @@ export function Presentation() {
     <div className="container" style={{ marginTop: 64 }}>
       <div className="row">
         <div className="col-md-6 col-sm-12">
-          <h3 style={{ fontWeight: "bold" }}>
+          <h3 style={{ fontWeight: "bold", fontSize: 48 }}>
             A melhor distribuidora do nordeste!
           </h3>
-          <div>
+          <div style={{
+            fontSize: 24,
+            marginTop: 16,
+          }}>
             Na Fort Distribuidora, oferecemos uma linha completa de materiais de
             construção para atender suas necessidades, desde pequenas reformas
             até grandes obras. Garantimos que você receba o que precisa, quando
             e onde for necessário.
-          </div>
-          <div className="row" style={{ marginTop: 16 }}>
-            <div className="col-6">
-              {/* <Item src={swimming} text="Piscina" />
-              <Item src={grill} text="Área da Churrasqueira" />
-              <Item src={steamroom} text="Sauna" />
-              <Item src={slider} text="Playground" /> */}
-            </div>
-
-            <div className="col-6">
-              {/* <Item src={fountain} text="Chafariz" />
-              <Item src={bed} text="10 Quartos" />
-              <Item src={spatula} text="Cozinha" />
-              <Item src={parking} text="Estacionamento" /> */}
-            </div>
           </div>
         </div>
 
